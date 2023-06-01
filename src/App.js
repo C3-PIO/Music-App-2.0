5038f77967236b8d894d293fdbd2ac4624385483
Chad <cpiotrovsky@att.net>
2023-06-01 10:00:15 -0400

    Added styling and minor functionality updates

--- src/App.js
+++ src/App.js
@@ -4,46 +4,56 @@ import axios from "axios";
 const DB_URL = "http://localhost:3001/todos";
 
 function App() {
+  // List state
   const [todoList, setTodoList] = useState([]);
+  // New task state
   const [toDoText, setToDoText] = useState("");
 
+  // Grab existing list when page first loads
   useEffect(() => {
     getToDos();
-    console.log(todoList);
   }, []);
 
+  // Grabs all list items from DB
   const getToDos = async () => {
-    const response = await axios.get(DB_URL);
-    setTodoList(response.data);
-  };
-
-  const addToDo = async (e) => {
-    e.preventDefault()
     try {
-      const response = await axios.post(DB_URL, { text: toDoText });
-      setTodoList(prev => [...prev, response.data])
-      setToDoText('')
+      const response = await axios.get(DB_URL);
+      setTodoList(response.data);
     } catch (err) {
       console.log(err);
     }
   };
 
+  // Add new item
+  const addToDo = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(DB_URL, { text: toDoText });
+      setTodoList((prev) => [...prev, response.data]);
+      setToDoText("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Delete an item
   const deleteToDo = async (id) => {
     try {
-      const response = await axios.delete(DB_URL + `/${id}`)
-      const newToDoList = todoList.filter(item => item._id !== id)
-      setTodoList(newToDoList)
+      const response = await axios.delete(DB_URL + `/${id}`);
+      const newToDoList = todoList.filter((item) => item._id !== id);
+      setTodoList(newToDoList);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   return (
     <div className="App">
-      <h1>To Do List!</h1>
+      <h1>Do Stuff List</h1>
       <br />
       <h4>Tasks</h4>
-      <form onSubmit={e => addToDo(e)}>
+      <div className="input">
+      <form onSubmit={(e) => addToDo(e)}>
         <input
           type="text"
           placeholder="Add new task..."
@@ -54,13 +64,25 @@ function App() {
         ></input>
         <button type="submit">Add</button>
       </form>
+      </div>
       <div>
         {todoList.map((todo) => {
           return (
-            <div key={todo.id}>
-              <input type='checkbox'></input>
-              {todo.text}
-              <button className='delete-button' onClick={()=> {deleteToDo(todo._id)}}>x</button>
+            <div className="todo" key={todo.id}>
+              <input
+                id={todo.id}
+                type="checkbox"
+                checked={todo.complete ? "checked" : ""}
+              ></input>
+              <label for={todo.id}> {todo.text} </label>
+              <button
+                className="delete-button"
+                onClick={() => {
+                  deleteToDo(todo._id);
+                }}
+              >
+                X
+              </button>
             </div>
           );
         })}
